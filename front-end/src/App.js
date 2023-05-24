85023b3d320b55bba336f73aae208267c3ec7e76
kdarji <kdarji@surekhatech.com>
2023-05-24 15:45:58 +0530

    creat a property card

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,19 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import TopNavbar from "./components/TopNavbar";
-import AddProperty from "./pages/AddProperty";
-import Home from "./pages/Home";
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import TopNavbar from './components/TopNavbar';
+import AddProperty from './pages/AddProperty';
+import Home from './pages/Home';
 
 const App = () => {
-    return (
-        <HashRouter>
-            <TopNavbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/add-property" element={<AddProperty />} />
-            </Routes>
-        </HashRouter>
-    );
+  return (
+    <HashRouter>
+      <TopNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-property" element={<AddProperty />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,27 +1,34 @@
-import * as React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getProperties } from "../redux/property/propertySlice";
-
+import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProperties } from '../redux/property/propertySlice';
+import PropertyCard from '../components/property/PropertyCard';
 
 const Home = () => {
-    const { properties } = useSelector((store) => store.propertyHandler);
-    const dispatch = useDispatch();
-    React.useEffect(() => {
-        dispatch(getProperties());
-    }, []);
+  const { properties } = useSelector((store) => store.propertyHandler);
+  const [propertyArray, setPropertyArray] = React.useState([]);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(getProperties());
+  }, []);
 
-    React.useEffect(() => {
-        console.log("propertiesproperties", properties);
-    }, [properties])
+  React.useEffect(() => {
+    console.log('propertiesproperties', properties);
+    const { status, data } = properties;
+    status === "OK" && setPropertyArray(data);
+  }, [properties]);
 
+  console.log("propertyArray",propertyArray);
+  return (
+    <>
+   
+      <h1>Hello From</h1>
+      <h2>Talk To Broker</h2>
 
-
-    return (
-        <>
-            <h1>Hello From</h1>
-            <h2>Talk To Broker</h2>
-        </>
-    );
+      {propertyArray && propertyArray.length > 0 && propertyArray.map((property) => {
+        return <PropertyCard propertyDetails={property}/>
+    })}
+    </>
+  );
 };
 
 export default Home;
